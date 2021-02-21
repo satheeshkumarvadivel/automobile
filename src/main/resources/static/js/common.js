4a4c4efd842f37e9ec08cdd29a520844b7f81f13
satvadiv <satvadiv@cisco.com>
2021-02-21 20:20:56 +0530

    updated static ip instead of localhost

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,7 +2,7 @@
   $(function () {
 
     $(document).ready(function () {
-      sessionStorage.setItem('api_host', 'http://localhost:8001');
+      sessionStorage.setItem('api_host', 'http://65.0.149.38');
       if (sessionStorage.getItem('username') == null || sessionStorage.getItem('username') != 'satheesh') {
         window.location.href = "/login";
       }
