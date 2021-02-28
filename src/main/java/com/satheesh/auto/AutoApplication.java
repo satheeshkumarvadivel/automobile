dd2bb53cde1dc860174b54faebcbcb1987e9ae14
satvadiv <satvadiv@cisco.com>
2021-02-28 19:59:06 +0530

    fixed war not deployed issue

--- src/main/java/com/satheesh/auto/AutoApplication.java
+++ src/main/java/com/satheesh/auto/AutoApplication.java
@@ -2,9 +2,16 @@ package com.satheesh.auto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AutoApplication {
+public class AutoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AutoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoApplication.class, args);
